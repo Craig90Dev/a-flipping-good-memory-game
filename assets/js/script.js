c2d6355368a43c6e583170727afe0f5d1dd86b54
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-12 12:18:33 +0000

    Feat: Add matching card statements and timeout functions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,26 @@ let firstCard, secondCard;
 function flipCard() {
   this.classList.toggle('flip');
   if (!hasFlipped) {
-    //first click
+    //First click
     hasFlipped = true;
     firstCard = this;
   } else {
-    //second click
+    //Second click
     hasFlipped = false;
     secondCard = this;
-    //do cards match?
-
-  }
+    //Do cards match?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      //It's a match!
+      firstCard.removeEventListener('click', flipCard)
+      secondCard.removeEventListener('click', flipCard)
+    } else {
+      //Not a match!
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1000);
+    }
+}
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-  
