9066ccca693ac1c515ebbf7868069a1c4ffe5f64
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-12 12:06:33 +0000

    Feat: Add JS to recognise first and second card clicks in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 const cards = document.querySelectorAll ('.memory-card');
 
+let hasFlipped = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
+  if (!hasFlipped) {
+    //first click
+    hasFlipped = true;
+    firstCard = this;
+  } else {
+    //second click
+    hasFlipped = false;
+    secondCard = this;
+    //do cards match?
+
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
