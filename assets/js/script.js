9536998f902ffd4e5e27fdb306daad4701fa9149
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-12 12:28:11 +0000

    Feat: Added refactoring to script.js for cleaner code reading

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,33 @@ function flipCard() {
     //First click
     hasFlipped = true;
     firstCard = this;
-  } else {
-    //Second click
-    hasFlipped = false;
-    secondCard = this;
-    //Do cards match?
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      //It's a match!
-      firstCard.removeEventListener('click', flipCard)
-      secondCard.removeEventListener('click', flipCard)
-    } else {
-      //Not a match!
-      setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      }, 1000);
-    }
+
+    return;
+  } 
+  
+  //Second click
+  hasFlipped = false;
+  secondCard = this;
+  //Do cards match?
+  checkForMatch();
 }
+
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1000);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
